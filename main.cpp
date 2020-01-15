7e01858176a0bc56bb7df84629094605a8aeadf9
charlesilcn <55978063+charlesilcn@users.noreply.github.com>
2020-01-15 10:01:18 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,7 +12,3 @@ pinMode(28 ,OUTPUT);
 pinMode(4,OUTPUT);
 pinMode(5,INPUT);
 }
-int MPU9250_SPI_Channel = 1;
-const int MPU9250_ADDR = MPU9250_SIGNAL_PATH_RESET;
-float _flag_MPU9250_LSB = 65.5;
-int MPU9250_SPI_Freq = 1000000;
